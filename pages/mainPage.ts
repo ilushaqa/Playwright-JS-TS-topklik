b67886d22c1fe99dd80285b1ffcd5469d3c1a01b
Ilya <ilya.originaly@gmail.com>
2025-02-28 13:52:28 +0300

    First commit, playwright config, auth test

---
+++ pages/mainPage.ts
@@ -0,0 +1,33 @@
+import {expect, Locator, Page} from "@playwright/test";
+import * as allure from "allure-js-commons";
+
+export class MainPage {
+
+    page: Page;
+    loginField: Locator;
+    passwordField: Locator;
+    loginButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.loginField = page.getByPlaceholder('логин');
+        this.passwordField = page.getByPlaceholder('пароль');
+        this.loginButton = page.getByRole('button', {name: 'Войти'})
+    }
+
+    async standOpen() {
+        await allure.step("Авторизация", async () => {
+            await this.page.goto("https://dev.topklik.online/");
+            await expect(this.page.getByText('Войдите в личный кабинет, чтобы начать расчет.')).toBeVisible();
+        });
+    }
+
+    async userAuthorization(login: string, password: string) {
+        await allure.step("Авторизация", async () => {
+            await this.loginField.fill(login);
+            await this.passwordField.fill(password);
+            await this.loginButton.click();
+            await expect(this.page.getByText('Калькулятор столешниц')).toBeVisible();
+        });
+    }
+}
---
+++ tests/e2e/auth.spec.ts
@@ -0,0 +1,13 @@
+import {test} from '@playwright/test';
+import {MainPage} from "../../pages/mainPage";
+
+let mainPage: MainPage;
+
+test.beforeEach(async ({page}) => {
+    mainPage = new MainPage(page);
+    await mainPage.standOpen();
+});
+
+test("Авторизация на сайте с правильными кредами", async () => {
+    await mainPage.userAuthorization('tester@inzhenerka.tech', 'LetsTest!');
+});
